4cf7d5ac85e03630b29ebef274e2141e178ff4da
Agatha-Ren <a1799509@student.adelaide.edu.au>
2022-05-24 14:04:28 +0800

    update main_lincls.py

--- main_lincls.py
+++ main_lincls.py
@@ -186,7 +186,9 @@ def main_worker(gpu, ngpus_per_node, args):
 
             args.start_epoch = 0
             msg = model.load_state_dict(state_dict, strict=False)
+            print(set(msg.missing_keys))
             assert set(msg.missing_keys) == {"%s.weight" % linear_keyword, "%s.bias" % linear_keyword}
+
 
             print("=> loaded pre-trained model '{}'".format(args.pretrained))
         else:
